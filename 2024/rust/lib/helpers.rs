2ae93b6c087408b0ddb253f76468463618b26b58
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2025-02-10 11:07:56 +0100

    move helpers to module

---
+++ 2024/rust/lib/helpers.rs
@@ -0,0 +1,63 @@
+use std::fs::{read_to_string, File};
+use std::io::{self, BufRead};
+use std::path::Path;
+pub fn read_column_data_to_vec(filename: &str) -> Vec<Vec<u32>> {
+    let mut ret = Vec::<Vec<u32>>::new();
+    let mut init: bool = true;
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines.map_while(Result::ok) {
+            let data = line.split_whitespace();
+            for (index, column) in data.enumerate() {
+                let column_val = column.parse().unwrap();
+                if init {
+                    let mut column_vec = Vec::<u32>::new();
+                    column_vec.push(column_val);
+                    ret.push(column_vec);
+                } else {
+                    ret.get_mut(index).unwrap().push(column_val);
+                }
+            }
+            init = false;
+        }
+    };
+    return ret;
+}
+pub fn read_ordered_list_data_to_vec(filename: &str) -> Vec<Vec<u32>> {
+    let mut ret = Vec::<Vec<u32>>::new();
+
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines.map_while(Result::ok) {
+            let mut data = line.split_whitespace();
+            let mut line_data: Vec<u32> = Vec::new();
+            'inner: loop {
+                match data.next() {
+                    Some(val) => {
+                        line_data.push(val.parse().unwrap());
+                    }
+                    None => break 'inner,
+                }
+            }
+            ret.push(line_data);
+        }
+    };
+    return ret;
+}
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+pub fn read_to_vec_vec_char(path: &str) -> Vec<Vec<char>> {
+    let mut ret = Vec::new();
+
+    if let Ok(lines) = read_lines(path) {
+        for line in lines.map_while(Result::ok) {
+            let chars: Vec<char> = line.chars().map(|c| c).collect();
+            ret.push(chars);
+        }
+    }
+
+    return ret;
+}
--- 2024/rust/main.rs
+++ 2024/rust/main.rs
@@ -5,6 +5,9 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::time::Instant;
 
+mod lib;
+use lib::helpers::*;
+
 // - binary string as array of operations
 #[test]
 fn test_check_if_calculation_is_doable() {
@@ -327,18 +330,7 @@ fn find_next_obstacle(
         }
     }
 }
-fn read_to_vec_vec_char(path: &str) -> Vec<Vec<char>> {
-    let mut ret = Vec::new();
 
-    if let Ok(lines) = read_lines(path) {
-        for line in lines.map_while(Result::ok) {
-            let chars: Vec<char> = line.chars().map(|c| c).collect();
-            ret.push(chars);
-        }
-    }
-
-    return ret;
-}
 fn find_and_fix_broken_book_updates(
     rules: &HashMap<i32, Vec<u16>>,
     input: &Vec<Vec<u16>>,
@@ -811,55 +803,6 @@ fn count_safe_reactor_reports(reports: &Vec<bool>) -> u32 {
         .filter(|element| **element)
         .collect::<Vec<_>>()
         .len() as u32;
-}
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-fn read_ordered_list_data_to_vec(filename: &str) -> Vec<Vec<u32>> {
-    let mut ret = Vec::<Vec<u32>>::new();
-
-    if let Ok(lines) = read_lines(filename) {
-        for line in lines.map_while(Result::ok) {
-            let mut data = line.split_whitespace();
-            let mut line_data: Vec<u32> = Vec::new();
-            'inner: loop {
-                match data.next() {
-                    Some(val) => {
-                        line_data.push(val.parse().unwrap());
-                    }
-                    None => break 'inner,
-                }
-            }
-            ret.push(line_data);
-        }
-    };
-    return ret;
-}
-fn read_column_data_to_vec(filename: &str) -> Vec<Vec<u32>> {
-    let mut ret = Vec::<Vec<u32>>::new();
-    let mut init: bool = true;
-    if let Ok(lines) = read_lines(filename) {
-        for line in lines.map_while(Result::ok) {
-            let data = line.split_whitespace();
-            for (index, column) in data.enumerate() {
-                let column_val = column.parse().unwrap();
-                if init {
-                    let mut column_vec = Vec::<u32>::new();
-                    column_vec.push(column_val);
-                    ret.push(column_vec);
-                } else {
-                    ret.get_mut(index).unwrap().push(column_val);
-                }
-            }
-            init = false;
-        }
-    };
-    return ret;
 }
 
 #[cfg(test)]
